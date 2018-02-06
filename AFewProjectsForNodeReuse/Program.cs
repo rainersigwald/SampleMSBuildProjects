0021b60a023b8dde2606593058910be8c1bb835b
Rainer Sigwald <raines@microsoft.com>
2018-02-06 15:34:01 -0600

    Add project files.

---
+++ AFewProjectsForNodeReuse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AFewProjectsForNodeReuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
